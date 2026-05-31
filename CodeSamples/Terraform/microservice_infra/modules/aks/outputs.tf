23aed98b7bdad2cf715439a8a7fde8fcb4c33540
DevOps Insiders <info@devopsinsiders.com>
2026-05-31 11:44:29 +0530

    Refactor code structure for improved readability and maintainability

--- CodeSamples/Terraform/microservice_infra/modules/aks/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "aks_ids" {
-  value = { for k, v in azurerm_kubernetes_cluster.aks : k => v.id }
-}
-
-output "aks_kube_configs" {
-  value     = { for k, v in azurerm_kubernetes_cluster.aks : k => v.kube_config_raw }
-  sensitive = true
-}
--- CodeSamples/Terraform/microservice_infra/modules/resource_group/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "resource_groups" {
-  description = "Map of resource groups to create"
-  type = map(object({
-    location = string
-    tags     = optional(map(string), {})
-  }))
-}
